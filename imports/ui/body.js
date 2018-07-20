b5e6976e5b876139bd6a43c6ed2329057a0fd055
Rohit Rajhans <rohitrajhans07@gmail.com>
2018-07-20 21:47:45 +0530

    user login feature added

--- imports/ui/body.js
+++ imports/ui/body.js
@@ -1,11 +1,23 @@
+import { Meteor } from 'meteor/meteor';
 import { Template } from 'meteor/templating';
+import { ReactiveDict } from 'meteor/reactive-dict';
 import { Tasks } from '../api/tasks.js';
 
-
+import './task.js';
 import './body.html';
+
+Template.body.onCreated( function bodyOnCreated() {
+    this.state = new ReactiveDict();
+});
+
 
 Template.body.helpers({
     tasks() {
+        const instance = Template.instance();
+        if (instance.state.get('hideTasks')) {
+            // if hide tasks is selected then display only those uncompleted tasks
+            return Tasks.find({ checked: {$ne: true}}, { sort: {createdAt: -1}});
+        }
         // console.log(Tasks.find({}));
         // console.log(Tasks.find({}));
         return Tasks.find({}, { sort: {createdAt: -1} });
@@ -25,9 +37,14 @@ Template.body.events({
 
         Tasks.insert({
             text,
-            createdAt: new Date()
+            createdAt: new Date(),
+            owner: Meteor.userId(),
+            username: Meteor.user().username
         });
 
         target.text.value = "";
+    },
+    'change .hide-tasks input'(event, instance) {
+        instance.state.set('hideTasks', event.target.checked);
     }
-})
+});
